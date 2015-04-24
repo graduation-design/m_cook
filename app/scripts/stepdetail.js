45f89baebbca79307e101810c04a2b9a56cf7771
lukeisme <lukeisme2013@gmail.com>
2015-04-24 17:36:54 +0800

    dev

--- app/scripts/stepdetail.js
+++ app/scripts/stepdetail.js
@@ -2,16 +2,32 @@
  * Created by Administrator on 2015/4/24.
  */
 
-$(function(){
+$(function () {
   var num = 1;
-    $(".step-pic").on("click",".selected", function(){
-      if(num < 3){
-        $(".selected+li").addClass("selected");
-        $(this).removeClass("selected");
+  $(".step-pic").on("click", ".selected", function () {
+    if (num < 3) {
+      $(".selected+li").addClass("selected");
+      $(this).removeClass("selected");
 
-        num = num+1;
-        $(".header").text(num+'/3')
+      num = num + 1;
+      $(".header").text(num + '/3')
+    }
+  });
+
+  $('.big-pic').hammer().on('swipeleft swiperight', function (ev) {
+    var curSelected = $('.selected');
+    if (ev.type === 'swipeleft') {
+      if (num < 3) {
+        $(".selected+li").addClass("selected");
+        $(curSelected).removeClass("selected");
+
+        num = num + 1;
+        $(".header").text(num + '/3')
       }
-    })
+    }
+    if (ev.type === 'swiperight') {
+
+    }
+  })
 
 });
