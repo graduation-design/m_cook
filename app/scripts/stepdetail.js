79015c6e5a9bf6cbb8e5b82c7895c21ac43e5b32
xdh <529200382@qq.com>
2015-04-27 15:03:50 +0800

    dev

--- app/scripts/stepdetail.js
+++ app/scripts/stepdetail.js
@@ -4,21 +4,13 @@
 
 $(function () {
   var num = 1;
-  $(".step-pic").on("click", ".selected", function () {
-    if (num < 3) {
-      $(".selected+li").addClass("selected");
-      $(this).removeClass("selected");
-
-      num = num + 1;
-      $(".header").text(num + '/3')
-    }
-  });
 
   $('.big-pic').hammer().on('swipeleft swiperight', function (ev) {
     var curSelected = $('.selected');
+    var stepItems = $('.step-pic li');
     if (ev.type === 'swipeleft') {
       if (num < 3) {
-        $(".selected+li").addClass("selected");
+        $(stepItems[curSelected.index() + 1]).addClass('selected');
         $(curSelected).removeClass("selected");
 
         num = num + 1;
@@ -26,7 +18,13 @@ $(function () {
       }
     }
     if (ev.type === 'swiperight') {
+      if (num > 1) {
+        $(stepItems[curSelected.index() - 1]).addClass('selected');
+        $(curSelected).removeClass("selected");
 
+        num = num - 1;
+        $(".header").text(num + '/3')
+      }
     }
   })
 
