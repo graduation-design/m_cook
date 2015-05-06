7b8bbb03050ee65debcf48af709e8a86c374e8e7
lukeisme <lukeisme2013@gmail.com>
2015-05-06 15:35:21 +0800

    dev

--- app/scripts/list.js
+++ app/scripts/list.js
@@ -5,7 +5,7 @@
     $('.list_section').on('click', 'li', function(ev){
       var $tgtItem = $(ev.currentTarget);
 
-      $('body').append(window.Android.callDetailPage($tgtItem.attr('data-id'), $tgtItem.attr('data-type')));
+      $('#test').html(window.Android.callDetailPage($tgtItem.attr('data-id'), $tgtItem.attr('data-type')));
     });
   })
 })(jQuery);
