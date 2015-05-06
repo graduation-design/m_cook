1c4374a6ed50e897a4b81b2fe61a3439b2fd9546
lukeisme <lukeisme2013@gmail.com>
2015-05-06 14:27:36 +0800

    dev

--- app/scripts/login_register.js
+++ app/scripts/login_register.js
@@ -2,13 +2,22 @@
   'use strict';
 
   $(function(){
+    var loginSection = $('#login');
+    var registerSection = $('#register');
+    loginSection.find('p').addClass('show');
+
     $('.switcher').on('click', function(ev){
       var $wrapper = $('.wrapper');
-      if($(ev.target).hasClass('to_register'))
+      if($(ev.target).hasClass('to_register')){
         $wrapper.css('transform', 'translateY(-100%)');
-      else
+        registerSection.find('p').addClass('show');
+        loginSection.find('p').removeClass('show');
+      }
+      else{
         $wrapper.css('transform', 'translateY(0)');
-
+        loginSection.find('p').addClass('show');
+        registerSection.find('p').removeClass('show');
+      }
     });
   })
 })(jQuery);
