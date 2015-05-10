dfb68af55016a533a99ece34c5b23ae1ef3e4f50
lukeisme <lukeisme2013@gmail.com>
2015-05-10 20:46:31 +0800

    dev

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ gulp.task('html', ['styles'], function () {
   return gulp.src('app/*.html')
     .pipe(assets)
     .pipe($.if('*.js', $.uglify()))
+    //.pipe($.if('*.js', $.babel()))
     .pipe($.if('*.css', $.csso()))
     .pipe(assets.restore())
     .pipe($.useref())
