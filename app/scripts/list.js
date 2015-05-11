3ac6c13c11de079894b6ce9a67dfe7de8647485c
lukeisme <lukeisme2013@gmail.com>
2015-05-11 15:10:11 +0800

    dev

--- app/scripts/list.js
+++ app/scripts/list.js
@@ -2,20 +2,13 @@
   'use strict';
 
   $(function(){
-    var listSection = $('.list_section');
-
-    listSection.on('touchstart', 'li', function(ev){
+    $('.list_section').on('click', 'li', function(ev){
       var $tgtItem = $(ev.currentTarget);
+
       $tgtItem.css('background-color', '#ccc');
-    });
-
-    listSection.on('touchend', 'li', function(ev){
-      var $tgtItem = $(ev.currentTarget);
-      $tgtItem.css('background-color', 'transparent');
-    });
-
-    listSection.on('click', 'li', function(ev){
-      var $tgtItem = $(ev.currentTarget);
+      setTimeout(function(){
+        $tgtItem.css('background-color', 'transparent');
+      },100);
 
       Android.callDetailPage($tgtItem.attr('data-id'), $tgtItem.attr('data-type'));
     });
