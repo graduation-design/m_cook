c9c79ba36b2a5d042a62ba96b48bc2330c90f0af
lukeisme <lukeisme2013@gmail.com>
2015-05-14 11:15:49 +0800

    friends_list

---
+++ app/scripts/friends_list.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+    $('.friends_list li').css({
+      '-webkit-transform': 'scale(1)',
+      'transform': 'scale(1)',
+      'opacity': 1
+    });
+  })
+}(jQuery));
---
+++ dist/script/friends_list.js
@@ -0,0 +1,1 @@
+"use strict";!function(s){s(function(){s(".friends_list li").css({"-webkit-transform":"scale(1)",transform:"scale(1)",opacity:1})})}(jQuery);
