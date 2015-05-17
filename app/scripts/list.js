9cf88a6fa68ad077ba73523c5efd441265876cf9
lukeisme <lukeisme2013@gmail.com>
2015-05-17 13:18:30 +0800

    dev

--- app/scripts/list.js
+++ app/scripts/list.js
@@ -1,32 +1,39 @@
-(function(){
+(function () {
   'use strict';
 
-  $(function(){
-    $('.list_section').on('click', 'li', function(ev){
+  $(function () {
+    $('.list_section').on('click', 'li', function (ev) {
       var $tgtItem = $(ev.currentTarget);
 
       $tgtItem.css('background-color', '#ccc');
-      setTimeout(function(){
+      setTimeout(function () {
         $tgtItem.css('background-color', 'transparent');
-      },100);
+      }, 100);
 
       Android.callDetailPage($tgtItem.attr('data-id'), $tgtItem.attr('data-type'));
     });
   });
 
-  $(window).on('load', function(){
+  $(window).on('load', function () {
+    var $banner = $('#banner');
     $('.loading_cover').fadeOut();
-    setTimeout(function(){
-      if($('#banner').length === 0) return;
 
-      $('.blink_text').addClass('play');
+    if ($banner.length !== 0){
+      $banner.on('click', function(ev){
+        var $tgtItem = $(ev.currentTarget);
 
-      setTimeout(function(){
-        //banner title to small
-        $('.banner_detail').addClass('small');
-        $('.cookbook_name').addClass('show');
-        $('.author').addClass('show');
-      },2000)
-    }, 1000);
+        Android.callDetailPage($tgtItem.attr('data-id'), $tgtItem.attr('data-type'));
+      });
+      setTimeout(function () {
+        $('.blink_text').addClass('play');
+
+        setTimeout(function () {
+          //banner title to small
+          $('.banner_detail').addClass('small');
+          $('.cookbook_name').addClass('show');
+          $('.author').addClass('show');
+        }, 2000)
+      }, 1000);
+    }
   })
 })(jQuery);
