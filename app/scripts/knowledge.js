76adb7b73a3396ff86cfd042abee51a4357326ac
lukeisme <lukeisme2013@gmail.com>
2015-05-18 09:58:34 +0800

    adjust

--- app/scripts/knowledge.js
+++ app/scripts/knowledge.js
@@ -1,34 +1,11 @@
 (function ($) {
   'use strict';
   $(function () {
-    toolbarScrollHandler();
     toolbarTap();
   });
 
-  var toolbarScrollHandler = function(){
-    var $toolbar = $('.social_toolbar');
-    var $toolbarList = $toolbar.find('ul');
-    var socialToolbarOffset = $toolbarList.find('li').offset();
-    var isFixed = false;
-    $(document).on('scroll', function () {
-      if (socialToolbarOffset.top - 12 < $(this).scrollTop()) {
-        if(isFixed) return;
-        $toolbarList.css({
-          'position': 'fixed',
-          'top': 12,
-          'left': socialToolbarOffset.left
-        });
-        isFixed = true
-      }else{
-        if(!isFixed) return;
-        $toolbarList.css('position', 'static');
-        isFixed = false;
-      }
-    });
-  };
-
-  var toolbarTap = function(){
-    $('.social_toolbar li').on('click', function(ev){
+  var toolbarTap = function () {
+    $('.social_toolbar li').on('click', function (ev) {
       $(ev.target).find('span').toggleClass('used');
     })
   }
